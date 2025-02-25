b55e41f3821916cf7a63f51fc56157948653f235
Henry Moran <virtualhenry@gmail.com>
2025-02-25 11:27:11 -0800

    - adding beam analytics to track visitors from submitting to macmenubar directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,40 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Momentry - Mac Date Tracker",
   description: "Track life's important moments right from your Mac's menu bar.",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <script
+        src="https://beamanalytics.b-cdn.net/beam.min.js"
+        data-token="25f94ca0-d8d0-4909-8882-9fbdac847a2b"
+        async
+      ></script>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
